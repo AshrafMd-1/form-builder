a10a38555ddd86e047ee6aff6aab3407b2f892c3
Ashraf Mohammed <21951A6612@iare.ac.in>
2023-08-20 16:15:41 +0530

    add form button clears search params

--- src/components/AllForms.tsx
+++ src/components/AllForms.tsx
@@ -5,54 +5,67 @@ export const AllForms = (prop: {
   forms: formInfo[];
   addFormCB: () => void;
   removeFormCB: (id: number) => void;
+  search: string;
 }) => {
   return (
     <div className="p-4 border  relative bg-gray-300 border-gray-300 mb-3 rounded-lg shadow-md">
       <div className="text-xl font-bold text-center">All Forms</div>
-      <div className="absolute top-0 right-0 mr-3 mt-3 flex">
+      <div className="absolute top-0 right-0 mr-3 mt-4 flex">
         <div className="text-xl font-bold text-center">Count :</div>
         <div className="text-xl font-bold text-center ml-2">
-          {prop.forms.length}
+          {
+            prop.forms.filter((form) => {
+              return form.title
+                .toLowerCase()
+                .includes(prop.search?.toLowerCase() || "");
+            }).length
+          }
         </div>
       </div>
       <div className="flex flex-col justify-center items-center">
-        <div className="flex flex-wrap m-2 justify-center">
-          {prop.forms.map((form: formInfo) => (
-            <div
-              key={form.id}
-              className="flex m-2 p-2 bg-blue-500 text-white items-center rounded-xl focus:outline-none focus:shadow-outline-blue"
-            >
-              <p className="text-l font-bold">{form.title}</p>
-              <div className="flex justify-center items-center">
-                <a
-                  href={"/forms/" + form.id}
-                  className="bg-amber-400 ml-2 rounded-lg hover:bg-amber-500  p-1"
-                >
-                  🖋️
-                </a>
-                <button
-                  className=" bg-red-500 ml-1 rounded-lg hover:bg-red-600 hover:text-white p-1"
-                  onClick={(_) => prop.removeFormCB(form.id)}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
+        <div className="flex flex-wrap w-full  m-2 justify-center">
+          {prop.forms
+            .filter((form) => {
+              return form.title
+                .toLowerCase()
+                .includes(prop.search?.toLowerCase() || "");
+            })
+            .map((form: formInfo) => (
+              <div
+                key={form.id}
+                className="flex m-2 p-2 w-full mx-5 justify-between bg-blue-500 text-white items-center rounded-xl focus:outline-none focus:shadow-outline-blue"
+              >
+                <p className="text-xl ml-3 font-bold">{form.title}</p>
+                <div className="flex justify-center items-center">
+                  <a
+                    href={"/forms/" + form.id}
+                    className="bg-amber-400 font-bold ml-2 rounded-lg hover:bg-amber-500 px-3 py-1 "
                   >
-                    <path d="M6 18L18 6M6 6l12 12" />
-                  </svg>
-                </button>
+                    EDIT
+                  </a>
+                  <button
+                    className=" bg-red-500 ml-2 rounded-lg hover:bg-red-600 hover:text-white p-1"
+                    onClick={(_) => prop.removeFormCB(form.id)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
         <button
           onClick={(_) => prop.addFormCB()}
           className="bg-green-500 ml-auto hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg mr-2 focus:outline-none focus:shadow-outline-green active:bg-green-800"
         >
-          Add Form
+          ➕ Add Form
         </button>
       </div>
     </div>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { AllForms } from "./AllForms";
 import { getLocalForms, initialFormFields, saveLocalForms } from "./utils";
+import { useQueryParams } from "raviger";
 
 const getAllForms = () => {
   const localForms = getLocalForms();
@@ -14,6 +15,8 @@ const getAllForms = () => {
 
 export default function Home() {
   const [state, setState] = useState(() => getAllForms());
+  const [{ search }, setQuery] = useQueryParams();
+  const [searchString, setSearchString] = useState("");
 
   const addForm = () => {
     const localForms = getLocalForms();
@@ -24,6 +27,8 @@ export default function Home() {
     };
     saveLocalForms([...localForms, newForm]);
     setState(getAllForms());
+    setSearchString("");
+    setQuery({ search: "" });
   };
 
   const removeForm = (id: number) => {
@@ -37,7 +42,29 @@ export default function Home() {
 
   return (
     <div className="flex flex-col justify-content">
-      <AllForms forms={state} addFormCB={addForm} removeFormCB={removeForm} />
+      <div className="flex flex-col mt-3 mb-3">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setQuery({ search: searchString });
+          }}
+        >
+          <label className="mr-2">Search</label>
+          <input
+            className="border-2 border-gray-300 bg-white h-10 px-5 pr-1 rounded-lg text-m focus:outline-none"
+            type="search"
+            name="search"
+            value={searchString}
+            onChange={(e) => setSearchString(e.target.value)}
+          />
+        </form>
+      </div>
+      <AllForms
+        forms={state}
+        addFormCB={addForm}
+        removeFormCB={removeForm}
+        search={search}
+      />
     </div>
   );
 }
