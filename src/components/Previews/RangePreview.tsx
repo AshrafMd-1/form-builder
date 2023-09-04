429ac78d9eebca9c80ef6c021343541191000e11
Ashraf Mohammed <21951A6612@iare.ac.in>
2023-09-05 00:30:09 +0530

    first commit

--- src/components/Previews/RangePreview.tsx
+++ src/components/Previews/RangePreview.tsx
@@ -1,27 +1,31 @@
 import React from "react";
 
 interface RangePreviewProps {
-  kind: string,
-  min: number,
-  max: number,
-  step: number,
-  inputValue: string,
+  kind: string;
+  min: number;
+  max: number;
+  step: number;
+  inputValue: string;
   setInputValueUsingECB: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const RangePreview = (props: RangePreviewProps) => {
+  console.log("RangePreview");
+  console.log(props.inputValue);
   return (
-      <>
-        <input
-            className="border-2 mt-2 border-gray-300 bg-white h-10 pl-1 rounded-lg text-m focus:outline-none invalid:border-red-500"
-            type="range"
-            min={props.min}
-            max={props.max}
-            step={props.step}
-            value={props.inputValue || 0}
-            onChange={(e) => props.setInputValueUsingECB(e)}
-        />
-        <label className="mr-2 font-bold">Value : {props.inputValue || 0}</label>
-      </>
-  )
-}
+    <>
+      <input
+        className="border-2 mt-2 border-gray-300 bg-white h-10 pl-1 rounded-lg text-m focus:outline-none invalid:border-red-500"
+        type="range"
+        min={props.min}
+        max={props.max}
+        step={props.step}
+        value={props.inputValue === "" ? props.min : props.inputValue}
+        onChange={(e) => props.setInputValueUsingECB(e)}
+      />
+      <label className="mr-2 font-bold">
+        Value : {props.inputValue === "" ? props.min : props.inputValue}
+      </label>
+    </>
+  );
+};
---
+++ src/types/formReducerTypes.ts
@@ -0,0 +1,70 @@
+type ChangeFormTitle = {
+  type: "change_form_title";
+  title: string;
+};
+
+type AddFieldToForm = {
+  type: "add_field_to_form";
+  fieldType: string;
+  value: string;
+  callback: ()=>void;
+};
+
+type RemoveFieldFromForm = {
+  type: "remove_field_from_form";
+  id: number;
+};
+
+type EditFormFieldLabel = {
+  type: "edit_form_field_label";
+  id: number;
+  label: string;
+};
+
+type EditFormFieldType = {
+  type: "edit_form_field_type";
+  id: number;
+  fieldType: string;
+};
+
+type AddOptionToFormField = {
+  type: "add_option_to_form_field";
+  id: number;
+  option: string;
+};
+
+type RemoveOptionFromFormField = {
+  type: "remove_option_from_form_field";
+  id: number;
+  index: number;
+};
+
+type EditRangeMinOfFormField = {
+  type: "edit_range_min_of_form_field";
+  id: number;
+  min: number;
+};
+
+type EditRangeMaxOfFormField = {
+  type: "edit_range_max_of_form_field";
+  id: number;
+  max: number;
+};
+
+type EditRangeStepOfFormField = {
+  type: "edit_range_step_of_form_field";
+  id: number;
+  step: number;
+};
+
+export type FormAction =
+  | ChangeFormTitle
+  | AddFieldToForm
+  | RemoveFieldFromForm
+  | EditFormFieldLabel
+  | EditFormFieldType
+  | AddOptionToFormField
+  | RemoveOptionFromFormField
+  | EditRangeMinOfFormField
+  | EditRangeMaxOfFormField
+  | EditRangeStepOfFormField;
