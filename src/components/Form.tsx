ec0faf4a7f828fe106dba25489ca48bef46e570e
Ashraf Mohammed <21951A6612@iare.ac.in>
2023-09-05 18:13:09 +0530

    change formatting

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,11 +5,9 @@ import {
   Options,
   saveFormData,
 } from "../utils/utils";
-
 import { formData, formField } from "../types/formTypes";
 import { Link } from "raviger";
 import { Error } from "./Error";
-
 import LabelledInputs from "./Inputs/LabelledInputs";
 import MultiSelectInputs from "./Inputs/MultiSelectInputs";
 import { RangeInputs } from "./Inputs/RangeInputs";
@@ -18,21 +16,14 @@ import { FormAction } from "../types/formReducerTypes";
 const getFormBasedOnID = (id: number) => {
   const localForms = getLocalForms();
   const form = localForms.find((form) => form.id === id);
-  return form
-    ? form
-    : {
-        id: 0,
-        title: "Sample Form",
-        formFields: [],
-      };
+  return form || { id: 0, title: "Sample Form", formFields: [] };
 };
 
 const reducer = (state: formData, action: FormAction) => {
   switch (action.type) {
-    case "change_form_title": {
+    case "change_form_title":
       return { ...state, title: action.title };
-    }
-    case "add_field_to_form": {
+    case "add_field_to_form":
       action.callback();
       return {
         ...state,
@@ -40,80 +31,95 @@ const reducer = (state: formData, action: FormAction) => {
           ? [...state.formFields, action.formField]
           : [...state.formFields],
       };
-    }
-    case "remove_field_from_form": {
-      const updatedFormFields = state.formFields.filter(
-        (field: formField) => field.id !== action.id,
-      );
-      return { ...state, formFields: updatedFormFields };
-    }
-    case "edit_form_field_label": {
-      const updatedFormFields = state.formFields.map((field: formField) =>
-        field.id === action.id ? { ...field, label: action.label } : field,
-      );
-      return { ...state, formFields: updatedFormFields };
-    }
-    case "add_option_to_form_field": {
-      if (!action.option) return state;
-
-      const updatedFormFields = state.formFields.map((field: formField) => {
-        if (
+    case "remove_field_from_form":
+      return {
+        ...state,
+        formFields: state.formFields.filter(
+          (field: formField) => field.id !== action.id,
+        ),
+      };
+    case "edit_form_field_label":
+      return {
+        ...state,
+        formFields: state.formFields.map((field: formField) =>
+          field.id === action.id
+            ? {
+                ...field,
+                label: action.label,
+              }
+            : field,
+        ),
+      };
+    case "add_option_to_form_field":
+      return {
+        ...state,
+        formFields: state.formFields.map((field: formField) =>
           (field.kind === "radio" || field.kind === "multi-select") &&
           field.id === action.id
-        ) {
-          const options = [...new Set([...field.options, action.option])];
-          return { ...field, options };
-        }
-        return field;
-      });
-
-      return { ...state, formFields: updatedFormFields };
-    }
-    case "remove_option_from_form_field": {
-      const updatedFormFields = state.formFields.map((field: formField) => {
-        if (
+            ? {
+                ...field,
+                options: [...new Set([...field.options, action.option])],
+              }
+            : field,
+        ),
+      };
+    case "remove_option_from_form_field":
+      return {
+        ...state,
+        formFields: state.formFields.map((field: formField) =>
           (field.kind === "radio" || field.kind === "multi-select") &&
           field.id === action.id
-        ) {
-          const options =
-            field.options.length === 1
-              ? ["Sample Option 1"]
-              : [...field.options];
-          options.splice(action.index, 1);
-          return { ...field, options };
-        }
-        return field;
-      });
-
-      return { ...state, formFields: updatedFormFields };
-    }
-    case "edit_range_min_of_form_field": {
-      const updatedFormFields = state.formFields.map((field: formField) =>
-        field.id === action.id && field.kind === "range"
-          ? { ...field, min: action.min }
-          : field,
-      );
-
-      return { ...state, formFields: updatedFormFields };
-    }
-    case "edit_range_max_of_form_field": {
-      const updatedFormFields = state.formFields.map((field: formField) =>
-        field.id === action.id && field.kind === "range"
-          ? { ...field, max: action.max }
-          : field,
-      );
-
-      return { ...state, formFields: updatedFormFields };
-    }
-    case "edit_range_step_of_form_field": {
-      const updatedFormFields = state.formFields.map((field: formField) =>
-        field.id === action.id && field.kind === "range"
-          ? { ...field, step: action.step }
-          : field,
-      );
-
-      return { ...state, formFields: updatedFormFields };
-    }
+            ? {
+                ...field,
+                options:
+                  field.options.length === 1
+                    ? ["Sample Option 1"]
+                    : [
+                        ...field.options.slice(0, action.index),
+                        ...field.options.slice(action.index + 1),
+                      ],
+              }
+            : field,
+        ),
+      };
+    case "edit_range_min_of_form_field":
+      return {
+        ...state,
+        formFields: state.formFields.map((field: formField) =>
+          field.id === action.id && field.kind === "range"
+            ? {
+                ...field,
+                min: action.min,
+              }
+            : field,
+        ),
+      };
+    case "edit_range_max_of_form_field":
+      return {
+        ...state,
+        formFields: state.formFields.map((field: formField) =>
+          field.id === action.id && field.kind === "range"
+            ? {
+                ...field,
+                max: action.max,
+              }
+            : field,
+        ),
+      };
+    case "edit_range_step_of_form_field":
+      return {
+        ...state,
+        formFields: state.formFields.map((field: formField) =>
+          field.id === action.id && field.kind === "range"
+            ? {
+                ...field,
+                step: action.step,
+              }
+            : field,
+        ),
+      };
+    default:
+      return state;
   }
 };
 
@@ -123,9 +129,7 @@ export default function Form(props: { formId: number }) {
   const titleRef = useRef<HTMLInputElement | null>(null);
 
   useEffect(() => {
-    console.log("Component Mounted");
     document.title = "Form Editor";
-
     titleRef.current?.focus();
     return () => {
       document.title = "React App";
@@ -133,7 +137,7 @@ export default function Form(props: { formId: number }) {
   }, []);
 
   useEffect(() => {
-    let timeout = setTimeout(() => {
+    const timeout = setTimeout(() => {
       saveFormData(state);
       console.log("State Saved To Local Storage");
     }, 1000);
@@ -152,105 +156,103 @@ export default function Form(props: { formId: number }) {
   }
 
   const renderInputTypes = (field: formField, index: number) => {
-    if (field.kind === "radio" || field.kind === "multi-select") {
-      return (
-        <MultiSelectInputs
-          key={field.id}
-          id={field.id}
-          label={field.label}
-          option={field.options}
-          kind={field.kind}
-          count={index + 1}
-          removeFieldCB={(id: number) =>
-            dispatch({ type: "remove_field_from_form", id: id })
-          }
-          columnChangeHandlerCB={(id: number, label: string) =>
-            dispatch({
-              type: "edit_form_field_label",
-              id: id,
-              label: label,
-            })
-          }
-          addOptionCB={(id: number, option: string) =>
-            dispatch({
-              type: "add_option_to_form_field",
-              id: id,
-              option: option,
-            })
-          }
-          removeOptionCB={(id: number, index: number) =>
-            dispatch({
-              type: "remove_option_from_form_field",
-              id: id,
-              index: index,
-            })
-          }
-        />
-      );
-    } else if (field.kind === "range") {
-      return (
-        <RangeInputs
-          key={field.id}
-          id={field.id}
-          label={field.label}
-          min={field.min}
-          max={field.max}
-          count={index + 1}
-          step={field.step}
-          kind={field.kind}
-          removeFieldCB={(id: number) =>
-            dispatch({ type: "remove_field_from_form", id: id })
-          }
-          columnChangeHandlerCB={(id: number, label: string) =>
-            dispatch({
-              type: "edit_form_field_label",
-              id: id,
-              label: label,
-            })
-          }
-          rangeMinChangeHandlerCB={(id: number, min: number) =>
-            dispatch({
-              type: "edit_range_min_of_form_field",
-              id: id,
-              min: min,
-            })
-          }
-          rangeMaxChangeHandlerCB={(id: number, max: number) =>
-            dispatch({
-              type: "edit_range_max_of_form_field",
-              id: id,
-              max: max,
-            })
-          }
-          rangeStepChangeHandlerCB={(id: number, step: number) =>
-            dispatch({
-              type: "edit_range_step_of_form_field",
-              id: id,
-              step: step,
-            })
-          }
-        />
-      );
-    } else {
-      return (
-        <LabelledInputs
-          key={field.id}
-          id={field.id}
-          label={field.label}
-          fieldType={field.fieldType}
-          count={index + 1}
-          removeFieldCB={(id: number) =>
-            dispatch({ type: "remove_field_from_form", id: id })
-          }
-          columnChangeHandlerCB={(id: number, label: string) =>
-            dispatch({
-              type: "edit_form_field_label",
-              id: id,
-              label: label,
-            })
-          }
-        />
-      );
+    switch (field.kind) {
+      case "radio":
+      case "multi-select":
+        return (
+          <MultiSelectInputs
+            key={field.id}
+            id={field.id}
+            label={field.label}
+            option={field.options}
+            kind={field.kind}
+            count={index + 1}
+            removeFieldCB={(id: number) =>
+              dispatch({ type: "remove_field_from_form", id })
+            }
+            columnChangeHandlerCB={(id: number, label: string) =>
+              dispatch({
+                type: "edit_form_field_label",
+                id,
+                label,
+              })
+            }
+            addOptionCB={(id: number, option: string) =>
+              dispatch({ type: "add_option_to_form_field", id, option })
+            }
+            removeOptionCB={(id: number, index: number) =>
+              dispatch({
+                type: "remove_option_from_form_field",
+                id,
+                index,
+              })
+            }
+          />
+        );
+      case "range":
+        return (
+          <RangeInputs
+            key={field.id}
+            id={field.id}
+            label={field.label}
+            min={field.min}
+            max={field.max}
+            count={index + 1}
+            step={field.step}
+            kind={field.kind}
+            removeFieldCB={(id: number) =>
+              dispatch({ type: "remove_field_from_form", id })
+            }
+            columnChangeHandlerCB={(id: number, label: string) =>
+              dispatch({
+                type: "edit_form_field_label",
+                id,
+                label,
+              })
+            }
+            rangeMinChangeHandlerCB={(id: number, min: number) =>
+              dispatch({
+                type: "edit_range_min_of_form_field",
+                id,
+                min,
+              })
+            }
+            rangeMaxChangeHandlerCB={(id: number, max: number) =>
+              dispatch({
+                type: "edit_range_max_of_form_field",
+                id,
+                max,
+              })
+            }
+            rangeStepChangeHandlerCB={(id: number, step: number) =>
+              dispatch({
+                type: "edit_range_step_of_form_field",
+                id,
+                step,
+              })
+            }
+          />
+        );
+      default:
+        return (
+          <LabelledInputs
+            key={field.id}
+            id={field.id}
+            label={field.label}
+            fieldType={field.fieldType}
+            count={index + 1}
+            removeFieldCB={(id: number) =>
+              dispatch({ type: "remove_field_from_form", id })
+            }
+            columnChangeHandlerCB={(id: number, label: string) =>
+              dispatch({
+                type: "edit_form_field_label",
+                id,
+                label,
+              })
+            }
+          />
+        );
     }
   };
 
@@ -264,10 +266,7 @@ export default function Form(props: { formId: number }) {
             className="border-2 mb-1 border-gray-300 rounded-md  mt-1 w-full h-10 px-2 text-lg focus:outline-none focus:border-blue-500"
             placeholder="New Form Title"
             onChange={(e) => {
-              dispatch({
-                type: "change_form_title",
-                title: e.target.value,
-              });
+              dispatch({ type: "change_form_title", title: e.target.value });
             }}
             ref={titleRef}
           />
@@ -282,37 +281,14 @@ export default function Form(props: { formId: number }) {
             className="border-2 border-gray-300 rounded-md  mt-1 w-full h-10 px-2 text-lg focus:outline-none focus:border-blue-500"
             placeholder="New Field Title"
             onChange={(e) => {
-              setNewField({
-                ...newField,
-                value: e.target.value,
-              });
+              setNewField({ ...newField, value: e.target.value });
             }}
           />
           <select
             className="border-2 border-gray-300 rounded-md  mt-1  h-10 px-2 text-lg focus:outline-none focus:border-blue-500"
             onChange={(e) => {
               const chosenInput = e.target.value;
-              if (chosenInput === "radio") {
-                setNewField({
-                  ...newField,
-                  fieldType: "radio",
-                });
-              } else if (chosenInput === "multi-select") {
-                setNewField({
-                  ...newField,
-                  fieldType: "multi-select",
-                });
-              } else if (chosenInput === "range") {
-                setNewField({
-                  ...newField,
-                  fieldType: "range",
-                });
-              } else {
-                setNewField({
-                  ...newField,
-                  fieldType: chosenInput,
-                });
-              }
+              setNewField({ ...newField, fieldType: chosenInput });
             }}
             value={newField.fieldType}
           >
@@ -321,38 +297,28 @@ export default function Form(props: { formId: number }) {
           <button
             className="mt-auto mb-auto ml-2 bg-blue-500 hover:bg-blue-600 text-white pl-3 pr-3 font-bold  rounded-lg focus:outline-none focus:shadow-outline-blue active:bg-blue-800"
             onClick={() => {
-              if (newField.fieldType === "radio") {
-                dispatch({
-                  type: "add_field_to_form",
-                  formField: {
+              let newForm: formField;
+              switch (newField.fieldType) {
+                case "radio":
+                  newForm = {
                     kind: "radio",
                     id: Number(new Date()),
                     label: newField.value,
                     options: ["Sample Option 1"],
                     value: "",
-                  },
-                  callback: () => {
-                    setNewField({ fieldType: "", value: "" });
-                  },
-                });
-              } else if (newField.fieldType === "multi-select") {
-                dispatch({
-                  type: "add_field_to_form",
-                  formField: {
+                  };
+                  break;
+                case "multi-select":
+                  newForm = {
                     kind: "multi-select",
                     id: Number(new Date()),
                     label: newField.value,
                     options: ["Sample Option 1"],
                     value: [],
-                  },
-                  callback: () => {
-                    setNewField({ fieldType: "", value: "" });
-                  },
-                });
-              } else if (newField.fieldType === "range") {
-                dispatch({
-                  type: "add_field_to_form",
-                  formField: {
+                  };
+                  break;
+                case "range":
+                  newForm = {
                     kind: "range",
                     id: Number(new Date()),
                     label: newField.value,
@@ -360,26 +326,22 @@ export default function Form(props: { formId: number }) {
                     max: 100,
                     step: 1,
                     value: 0,
-                  },
-                  callback: () => {
-                    setNewField({ fieldType: "", value: "" });
-                  },
-                });
-              } else {
-                dispatch({
-                  type: "add_field_to_form",
-                  formField: {
+                  };
+                  break;
+                default:
+                  newForm = {
                     kind: "text",
                     id: Number(new Date()),
                     label: newField.value,
                     fieldType: newField.fieldType,
                     value: "",
-                  },
-                  callback: () => {
-                    setNewField({ fieldType: "", value: "" });
-                  },
-                });
+                  };
               }
+              dispatch({
+                type: "add_field_to_form",
+                formField: newForm,
+                callback: () => setNewField({ fieldType: "text", value: "" }),
+              });
             }}
           >
             Add Field
@@ -387,9 +349,7 @@ export default function Form(props: { formId: number }) {
         </div>
         <div className="flex justify-end">
           <button
-            onClick={(_) => {
-              saveFormData(state);
-            }}
+            onClick={() => saveFormData(state)}
             className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg mr-2 focus:outline-none focus:shadow-outline-green active:bg-green-800"
           >
             Save
